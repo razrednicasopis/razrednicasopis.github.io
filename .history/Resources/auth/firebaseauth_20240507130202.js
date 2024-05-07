6a47336e6ab2ef2f2e892b424e76ac0805ce8515
razrednicasopis <razrednicasopis1kab@gmail.com>
2024-05-07 13:26:54 +0200

    Preparing for version 2.0

---
+++ .history/Resources/auth/firebaseauth_20240507130202.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC_Fw12d6WR9GFVt7TVKrFMkp4EFW8gijk",
+  authDomain: "razrednicasopisdatabase-29bad.firebaseapp.com",
+  projectId: "razrednicasopisdatabase-29bad",
+  storageBucket: "razrednicasopisdatabase-29bad.appspot.com",
+  messagingSenderId: "294018128318",
+  appId: "1:294018128318:web:31df9ea055eec5798e81ef"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ .history/Resources/auth/firebaseauth_20240507131731.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+import { getAuth, createUserWIthEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
+import { getFirestore, setDoc, doc } = from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC_Fw12d6WR9GFVt7TVKrFMkp4EFW8gijk",
+  authDomain: "razrednicasopisdatabase-29bad.firebaseapp.com",
+  projectId: "razrednicasopisdatabase-29bad",
+  storageBucket: "razrednicasopisdatabase-29bad.appspot.com",
+  messagingSenderId: "294018128318",
+  appId: "1:294018128318:web:31df9ea055eec5798e81ef"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
