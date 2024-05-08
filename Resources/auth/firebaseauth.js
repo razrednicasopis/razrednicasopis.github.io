18c12b2264772fab141e62a0468e8d535150a540
Dark <shadowfightvidsofficial@gmail.com>
2024-05-08 23:15:40 +0200

    Preparing for version 2.0

--- Resources/auth/firebaseauth.js
+++ Resources/auth/firebaseauth.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
 import { getAuth, createUserWIthEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
 import { getFirestore, setDoc, doc } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js";
+import { sign } from "jsonwebtoken";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,4 +18,41 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-const signUp = document.getElementById('registracijaBtn')
+
+const signUp = document.getElementById('registracijaBtn');
+signUp.addEventListener('click', (event) => {
+  event.preventDefault();
+  const username = document.getElementById('register-username').value;
+  const email = document.getElementById('register-email').value;
+  const password = document.getElementById('register-password').value
+
+  const auth = getAuth();
+  const db = getFirestore();
+
+  createUserWIthEmailAndPassword( auth, username, email, password)
+  .then((userCredential) => {
+    const user = userCredential.user;
+    const userData = {  
+      Username: username,
+      Email: email
+    };
+    alert('Račun uspešno ustvarjen! Sedaj se lahko prijavite.');
+    const docRef = doc(db, "users", user.uid);
+    setDoc(docRef, userData)
+    .then(() => {
+        window.location.href='prijava.html';
+    })
+    .catch((error) => {
+      console.error("Error writting the document:", error)
+    })
+  })
+  .catch ((error) => {
+    const errorCode = error.code;
+    if(errorCode == 'auth/email-already-in-use'){
+      alert('Ta e-mail račun je že v uporabi. Prosimo poskusite drug račun.');
+    }
+    else {
+      alert('Napaka pri ustvarjanju računa. Prosimo kontaktirajte našo pomoč.');
+    }
+  })
+});
