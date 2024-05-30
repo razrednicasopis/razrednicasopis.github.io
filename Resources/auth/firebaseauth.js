f59dfd12613cad7d9f88e208ac9ddefe9f807614
Dark <shadowfightvidsofficial@gmail.com>
2024-05-30 15:41:04 +0200

    Version 2.0

--- Resources/auth/firebaseauth.js
+++ Resources/auth/firebaseauth.js
@@ -1,8 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
 import { getFirestore, setDoc, doc } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -16,81 +14,68 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const db = getFirestore();
 
-
-const signUp = document.getElementById('registracijaBtn');
-signUp.addEventListener('click', async (event) => {
-  event.preventDefault();
-  const username = document.getElementById('register-username').value;
-  const email = document.getElementById('register-email').value;
-  const password = document.getElementById('register-password').value;
-
-  const auth = getAuth();
-  const db = getFirestore();
-
-  createUserWithEmailAndPassword( auth, email, password)
-  .then((userCredential) => {
-    const user = userCredential.user;
-    const userData = {
-      Username: username,
-      Email: email
-    };
-    alert('Račun uspešno ustvarjen! Sedaj se lahko prijavite.');
-    const docRef = doc(db, "users", user.uid);
-    setDoc(docRef, userData)
-    .then(() => {
-        window.location.href='prijava.html';
-    })
-    .catch((error) => {
-      console.error("Error writting the document:", error)
-    })
-  })
-  .catch ((error) => {
-    const errorCode = error.code;
-    if(errorCode == 'auth/email-already-in-use'){
-      alert('Ta e-mail račun je že v uporabi. Prosimo poskusite drug račun.');
-    }
-    else {
-      console.error("Prišlo je do napake pri ustvarjanju računa:", error);
-      alert('Napaka pri ustvarjanju računa. Prosimo kontaktirajte našo pomoč.');
-    }
-  })
-});
-
-
+// Registration
 document.addEventListener('DOMContentLoaded', () => {
-  console.log("DOM fully loaded and parsed");
-  
-  const loginBtn = document.getElementById('prijavaBtn');
-  if (!loginBtn) {
-    console.error('Login button not found.');
-    return;
+  const signUpBtn = document.getElementById('registracijaBtn');
+  if (signUpBtn) {
+    signUpBtn.addEventListener('click', async (event) => {
+      event.preventDefault();
+      const username = document.getElementById('register-username').value;
+      const email = document.getElementById('register-email').value;
+      const password = document.getElementById('register-password').value;
+
+      try {
+        const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+        const user = userCredential.user;
+        const userData = { Username: username, Email: email };
+
+        await setDoc(doc(db, "users", user.uid), userData);
+        alert('Račun uspešno ustvarjen! Sedaj se lahko prijavite.');
+        window.location.href = 'prijava.html';
+      } catch (error) {
+        if (error.code === 'auth/email-already-in-use') {
+          alert('Ta e-mail račun je že v uporabi. Prosimo poskusite drug račun.');
+        } else {
+          if (error.code === 'auth/invalid-email') {
+            alert('E-mail naslov ni veljaven.');
+          } else if (error.code === 'auth/weak-password') {
+            alert('Geslo je prešibko. Prosimo, izberite močnejše geslo.');
+          } else {
+            console.error("Prišlo je do napake pri ustvarjanju računa:", error);
+            alert('Napaka pri ustvarjanju računa. Prosimo kontaktirajte našo pomoč.');
+          }
+        }
+      }
+    });
   }
 
-  loginBtn.addEventListener('submit', async (event) => {
-    event.preventDefault();
-    console.log('Login button clicked');
-    
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    const auth = getAuth();
+  // Login
+  const loginBtn = document.getElementById('prijavaBtn');
+  if (loginBtn) {
+    loginBtn.addEventListener('click', async (event) => {
+      event.preventDefault();
+      const email = document.getElementById('email').value;
+      const password = document.getElementById('password').value;
 
-    try {
-      const userCredential = await signInWithEmailAndPassword(auth, email, password);
-      // Successfully logged in
-      const user = userCredential.user;
-      alert('Prijava uspešna!');
-      window.location.href = 'index.html'; // Redirect to the dashboard or desired page
-    } catch (error) {
-      const errorCode = error.code;
-      if (errorCode === 'auth/wrong-password') {
-        alert('Napačno geslo. Prosimo, poskusite znova.');
-      } else if (errorCode === 'auth/user-not-found') {
-        alert('Uporabnik s tem e-mail naslovom ne obstaja. Prosimo, preverite e-mail ali se registrirajte.');
-      } else {
-        console.error("Prišlo je do napake pri prijavi:", error);
-        alert('Napaka pri prijavi. Prosimo kontaktirajte našo pomoč.');
+      try {
+        const userCredential = await signInWithEmailAndPassword(auth, email, password);
+        alert('Prijava uspešna!');
+        window.location.href = 'index.html'; // Redirect to the dashboard or desired page
+      } catch (error) {
+        if (error.code === 'auth/wrong-password') {
+          alert('Napačno geslo ali e-mail račun. Prosimo poskusite znova.');
+        } else if (error.code === 'auth/user-not-found') {
+          alert('Uporabnik s tem e-mail naslovom ne obstaja. Prosimo, preverite e-mail ali se registrirajte.');
+        } else if (error.code === 'auth/invalid-email') {
+          alert('Napačno geslo ali e-mail račun. Prosimo poskusite znova.');
+        } else {
+          console.error("Prišlo je do napake pri prijavi:", error);
+          alert('Napaka pri prijavi. Prosimo kontaktirajte našo pomoč.');
+        }
       }
-    }
-  });
+    });
+  }
 });
