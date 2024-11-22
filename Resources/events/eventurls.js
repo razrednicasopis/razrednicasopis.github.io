f0af39cc9df1bcb0671ba4b57b49e7c45d1359f3
Dark <shadowfightvidsofficial@gmail.com>
2024-11-22 01:11:28 +0100

    Version 3.0

--- Resources/events/eventurls.js
+++ Resources/events/eventurls.js
@@ -3,9 +3,9 @@ export const eventUrls = {
       "https://razrednicasopis.github.io/Resources/events/hitrotipkanje/domov.html", // Live URL
       "http://127.0.0.1:5500/Resources/events/hitrotipkanje/domov.html" // Localhost URL
   ],
-  "Igra Ugibanja": [
-    "http://127.0.0.1:5500/Resources/events/igra_ugibanja/test.html", 
-    "url4"
+  "Festival Iger": [
+    "http://127.0.0.1:5500/Resources/events/festival_iger/domov.html", 
+    "http://127.0.0.1:5500/Resources/events/festival_iger/igre/slytherio.html"
   ], // Add your specific URLs for guessingGame event
   // Add other events and their corresponding URLs here
 };
